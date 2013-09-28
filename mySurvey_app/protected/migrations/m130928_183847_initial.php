7c9469fb84f754d0f910d5a48cd13a2e99c6ccb5
fwolf <fwolf@bu.edu>
2013-09-28 19:20:36 -0400

    switched over from sqlite to mysql.

---
+++ mySurvey_app/protected/migrations/m130928_183847_initial.php
@@ -0,0 +1,41 @@
+<?php
+
+class m130928_183847_initial extends CDbMigration
+{
+	public function up()
+	{
+                $this->createTable('survey_creator', array(
+                        'id' => 'pk',
+                        'username'=> 'VARCHAR(45) NOT NULL',
+                        'email' => 'VARCHAR(45) NOT NULL', 
+                        'password' => 'VARCHAR(45) NOT NULL',
+                        'first_name'=> 'VARCHAR(45) NULL',
+                        'last_name'=> 'VARCHAR(45) NULL',
+                ));
+                $this->insert(survey_creator, array(
+                    'username'=>'test1',
+                    'email'=>'test1@example.com',
+                    'password'=>'pass1',
+                    'first_name'=>'test',
+                    'last_name'=>'one'
+                ));
+                return true;
+	}
+
+	public function down()
+	{
+		$this->dropTable('survey_creator');
+                return true;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
