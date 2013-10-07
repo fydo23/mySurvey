450e7c6e461a7683871be0ae08a475c6247f553d
dualv <dualv@bu.edu>
2013-10-07 00:33:21 -0400

    login now uses email instead of username

--- mySurvey_app/protected/components/UserIdentity.php
+++ mySurvey_app/protected/components/UserIdentity.php
@@ -7,6 +7,8 @@
  */
 class UserIdentity extends CUserIdentity
 {
+	public $email;
+	private $_id;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -15,17 +17,27 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
+	public function __construct($email,$password){
+		$this->email=$email;
+		parent::__construct('', $password); 
+	}
 	public function authenticate()
 	{
                 $user = SurveyCreator::model()->findByAttributes(array(
-                    'username'=>$this->username,
+                    'email'=>$this->email,
                 ));
 		if(!$user)
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
 		elseif($user->password !== $this->password)
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
+		else{
+			$this->username=$user->username;
+			$this->_id=$user->id;
 			$this->errorCode=self::ERROR_NONE;
+		}
 		return !$this->errorCode;
+	}
+	public function getId(){
+		return $this->_id;
 	}
 }
