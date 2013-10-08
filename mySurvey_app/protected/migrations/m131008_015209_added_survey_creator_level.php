d0b3b67c5f89232c17c2f31dce301394b021a88a
fwolf <fwolf@bu.edu>
2013-10-08 19:56:06 -0400

    added level to surveyCreator.

---
+++ mySurvey_app/protected/migrations/m131008_015209_added_survey_creator_level.php
@@ -0,0 +1,26 @@
+<?php
+
+class m131008_015209_added_survey_creator_level extends CDbMigration
+{
+	public function up()
+	{
+            $this->addColumn('survey_creator', 'level', 'INT default 0');
+	}
+
+	public function down()
+	{
+            $this->dropColumn('survey_creator', 'level');
+            return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
