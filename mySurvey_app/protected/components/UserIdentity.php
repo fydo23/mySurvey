e237106ea3ef1352cfd7282023c5948e9cc2f79b
dualv <dualv@bu.edu>
2013-10-08 00:02:33 -0400

    register functions complete

--- mySurvey_app/protected/components/UserIdentity.php
+++ mySurvey_app/protected/components/UserIdentity.php
@@ -9,6 +9,7 @@ class UserIdentity extends CUserIdentity
 {
 	public $email;
 	private $_id;
+	const ERROR_EMAIL_DUPLICATE=3;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -37,6 +38,22 @@ class UserIdentity extends CUserIdentity
 		else{
 			$this->username=$user->username;
 			$this->_id=$user->id;
+			$this->errorCode=self::ERROR_NONE;
+		}
+		return !$this->errorCode;
+	}
+	public function authenticateRegister()
+	{
+		/*
+		 * find user with the same email and password
+		 * password is hashed using mysql password() function
+		*/
+		$user = SurveyCreator::model()->findByAttributes(
+        	array('email'=>$this->email)
+        );
+		if($user)
+			$this->errorCode=$this::ERROR_EMAIL_DUPLICATE;
+		else{
 			$this->errorCode=self::ERROR_NONE;
 		}
 		return !$this->errorCode;
--- mySurvey_app/protected/controllers/SiteController.php
+++ mySurvey_app/protected/controllers/SiteController.php
@@ -109,6 +109,26 @@ class SiteController extends Controller
 	public function actionRegister()
 	{
 		$model=new RegisterForm;
+		// if it is ajax validation request
+		if(isset($_POST['ajax']) && $_POST['ajax']==='register-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+		// collect user input data
+		if(isset($_POST['RegisterForm']))
+		{
+			$model->attributes=$_POST['RegisterForm'];
+			// validate user input and redirect to the previous page if valid
+			if($model->validate() && $model->Register()){
+			// log the user in
+				$loginModel=new LoginForm();
+				$loginModel->email=$model->email;
+				$loginModel->password=$model->password;
+				if($loginModel->validate() && $loginModel->login())
+				$this->redirect(Yii::app()->user->returnUrl);
+			}
+		}
 		$this->render('register',array('model'=>$model));
 	}
 
