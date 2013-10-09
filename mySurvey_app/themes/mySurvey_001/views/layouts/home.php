9f6cb3955a2d56dd4709619c506eb571273e23ea
gjensen01 <gloriajensen@Glorias-MacBook-Pro.local>
2013-10-09 00:22:23 -0400

    Create homepage per wireframe.
    Login & registration widgets need to be added.

---
+++ mySurvey_app/themes/mySurvey_001/views/layouts/home.php
@@ -0,0 +1,35 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/static/css/main.css" />
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+<div class="container" id="page">
+
+	<div id="header">
+		<div class="makeCenter"><img id="appLogo" src="http://localhost/static/img/logo.png"></div>
+	</div><!-- header -->
+
+                
+        <div id="content">
+            <?php echo $content; ?>
+        </div>
+                
+
+	<div id="footer">
+		<p>Footer content tk...</p>
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+</body>
+</html>
--- mySurvey_app/themes/mySurvey_001/views/site/index.php
+++ mySurvey_app/themes/mySurvey_001/views/site/index.php
@@ -1,20 +1,64 @@
+<?php Yii::app()->controller->layout = 'home'?> 
+
 <?php
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Lorem ipsum headline goes here.</h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed nulla ultricies, pellentesque felis nec, fringilla augue. Sed rutrum in nibh a mollis.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<div id="login-logout">
+	<a href="#" class="button">Sign up</a>
+	<p>or <a href="#" id="sign-in">Sign in</a></p>
+		
+</div>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div id="learn-more">
+	<p><a class="learn" href="#about">learn more</a></p>
+</div>
+
+<div id="about"> 
+	
+	<div id="what-it-is">
+		<div class="text">
+			<h1>Surveys made easy</h1>
+			<p>Nullam et mollis neque, egestas faucibus urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras nec turpis et nisl vestibulum aliquam pharetra a dui.</p>
+		</div>
+		<div class="image">
+			<img src="http://localhost/static/img/placeholder.gif">
+		</div>		
+	</div>
+	
+	
+	<div id="feature-1">	
+		<div class="image">
+			<img src="http://localhost/static/img/placeholder.gif">
+		</div>				
+		<div class="text">
+			<h1>Wherever you are</h1>
+			<p>Nullam et mollis neque, egestas faucibus urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras nec turpis et nisl vestibulum aliquam pharetra a dui.</p>
+		</div>				
+	</div>
+	
+	
+	<div id="feature-2">
+		<div class="text">
+			<h1>Analyzing results</h1>
+			<p>Nullam et mollis neque, egestas faucibus urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras nec turpis et nisl vestibulum aliquam pharetra a dui.</p>
+		</div>
+		<div class="image">
+			<img src="http://localhost/static/img/placeholder.gif">
+		</div>		
+	</div>
+	
+		
+</div>
+
+<div id="signup-button">
+		<a href="#" class="button">Sign up</a>
+	</div>
+
+
