8a87cead45b0294402595f0b1d1f474336ad8cde
fwolf <fwolf@bu.edu>
2013-10-09 23:57:58 -0400

    re-added survey crud form views.

---
+++ mySurvey_app/protected/views/surveyCreator/_search.php
@@ -0,0 +1,45 @@
+<?php
+/* @var $this SurveyCreatorController */
+/* @var $model SurveyCreator */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>45,'maxlength'=>45)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>45,'maxlength'=>45)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'first_name'); ?>
+		<?php echo $form->textField($model,'first_name',array('size'=>45,'maxlength'=>45)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'last_name'); ?>
+		<?php echo $form->textField($model,'last_name',array('size'=>45,'maxlength'=>45)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
