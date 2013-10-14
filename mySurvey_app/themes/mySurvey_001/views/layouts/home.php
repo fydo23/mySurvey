c1f1155d66c63cee4bec3a594028072a92421c01
gjensen01 <gloriajensen@Glorias-MacBook-Pro.local>
2013-10-14 09:49:42 -0400

    Add web fonts

--- mySurvey_app/themes/mySurvey_001/views/layouts/home.php
+++ mySurvey_app/themes/mySurvey_001/views/layouts/home.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" id="home">
 <head>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
