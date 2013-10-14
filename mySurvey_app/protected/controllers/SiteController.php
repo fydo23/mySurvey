59bf49a10c8af42e51065a4fb101e314ac5850af
fwolf <fwolf@bu.edu>
2013-10-13 23:09:16 -0400

    added access rules for survey- and site- controller pages.

--- mySurvey_app/protected/controllers/SiteController.php
+++ mySurvey_app/protected/controllers/SiteController.php
@@ -2,6 +2,36 @@
 
 class SiteController extends Controller
 {
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+                        array('allow',
+                                'actions'=>array('index', 'register', 'login'),
+                                'users'=>array('?'),
+                        ),
+			array('deny',
+				'users'=>array('?'),
+			),
+		);
+	}
+        
+        
 	/**
 	 * Declares class-based actions.
 	 */
--- mySurvey_app/protected/controllers/SurveyController.php
+++ mySurvey_app/protected/controllers/SurveyController.php
@@ -9,7 +9,7 @@ class SurveyController extends Controller
 	public function filters()
 	{
 		return array(
-//			'accessControl', // perform access control for CRUD operations
+			'accessControl', // perform access control for CRUD operations
 			'postOnly + delete', // we only allow deletion via POST request
 		);
 	}
@@ -22,20 +22,8 @@ class SurveyController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
+			array('deny', 
+                            'users'=>array('?'),
 			),
 		);
 	}
