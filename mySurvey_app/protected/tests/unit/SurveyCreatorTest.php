dd32f53a18892ba22efe83a933d10486951080d4
dualv <dualv@bu.edu>
2013-10-14 22:37:39 -0400

    added update test case for SurveyCreator model

--- mySurvey_app/protected/tests/unit/SurveyCreatorTest.php
+++ mySurvey_app/protected/tests/unit/SurveyCreatorTest.php
@@ -1,7 +1,9 @@
 <?php
 class SurveyCreatorTest extends CDbTestCase{
 	public function testInsert(){
+		//creates a new user for register
 		$surveyCreator=new SurveyCreator('register');
+		//set the attributes with dummy data
 		$surveyCreator->setAttributes(array(
 			'email'=>'testCase@example.com',
 			'password'=>'testpswd',
@@ -10,12 +12,24 @@ class SurveyCreatorTest extends CDbTestCase{
 			'last_name'=>'testLast',
 			'level'=>0,
 		));
+		//asserts the insert is success, and can be retrieved
 		$this->assertTrue($surveyCreator->save());
 		$this->assertNotNull($surveyCreator::model()->findByAttributes(array('email'=>$surveyCreator->email,'password'=>$surveyCreator->password)));
 	}
-	public function testDelete(){
+	public function testUpdate(){
+		//load the user data from db
 		$surveyCreator=new SurveyCreator();
-		$this->assertSame($surveyCreator::model()->deleteAllByAttributes(array('email'=>'testCase@example.com')),1);
+		$surveyCreator=$surveyCreator::model()->findByAttributes(array('email'=>'testCase@example.com','password'=>sha1('testpswd')));
+		//change the email
+		$surveyCreator->email='testCase2@example.com';
+		//assert the update is success and can be retrieved
+		$this->assertTrue($surveyCreator->save());
+		$this->assertNotNull($surveyCreator::model()->findByAttributes(array('email'=>$surveyCreator->email,'password'=>sha1('testpswd'))));
+	}
+	public function testDelete(){
+		//assert the deletion is successful
+		$surveyCreator=new SurveyCreator();
+		$this->assertSame($surveyCreator::model()->deleteAllByAttributes(array('email'=>'testCase2@example.com')),1);
 	}
 }
 ?>
