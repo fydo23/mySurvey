f5afda8c54c2ef4a2054ee1c626074074ade287d
Fyodor Wolf <fwolf@bu.edu>
2013-10-18 12:07:54 -0400

    Answer forms are visible.

---
+++ mySurvey_app/protected/views/answer/create.php
@@ -0,0 +1,9 @@
+<?php
+/* @var $this SurveyAnswerController */
+/* @var $model SurveyAnswer */
+
+?>
+
+<h1>Create SurveyAnswer</h1>
+
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>
