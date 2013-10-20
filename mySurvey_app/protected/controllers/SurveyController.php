b18bd6826045e5d17a04c55bf638e9e71985025c
fwolf <fwolf@bu.edu>
2013-10-20 15:38:37 -0400

    removed admin, search and view all surveys views from theme folder.

--- mySurvey_app/protected/controllers/SurveyController.php
+++ mySurvey_app/protected/controllers/SurveyController.php
@@ -167,21 +167,6 @@ class SurveyController extends Controller
 	}
 
 	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new Survey('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Survey']))
-			$model->attributes=$_GET['Survey'];
-
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
-
-	/**
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
 	 * @param integer $id the ID of the model to be loaded
