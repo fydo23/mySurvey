707b0bbe41067228170eec492aa97fbce7a00a32
bethh <bethh@bu.edu>
2013-10-21 15:27:48 -0400

    Adding test migrations to drop foreign keys

---
+++ mySurvey_app/protected/migrations/m131021_150157_drop_foreign_key_answer.php
@@ -0,0 +1,30 @@
+<?php
+
+class m131021_150157_drop_foreign_key_answer extends CDbMigration
+{
+	public function up()
+	{
+		$this->dropForeignKey('survey_answer_ibfk_1', 'survey_answer');
+		$this->dropIndex('survey_ID', 'survey_answer');
+		$this->dropColumn('survey_answer', 'survey_ID');
+	}
+
+	public function down()
+	{
+		$this->addColumn('survey_answer', 'survey_id', 'int(11) not null');
+		$this->addForeignKey('survey_answer_ibfk_1', 'survey_answer', 'survey_id', 'survey', 'id','CASCADE','CASCADE');
+		$this->createIndex('survey_id', 'survey_answer', 'survey_ID');
+
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
---
+++ mySurvey_app/protected/migrations/m131021_151154_drop_foreign_keys_response.php
@@ -0,0 +1,39 @@
+<?php
+
+class m131021_151154_drop_foreign_keys_response extends CDbMigration
+{
+	public function up()
+	{
+		$this->dropForeignKey('survey_response_ibfk_1', 'survey_response');
+		$this->dropIndex('survey_ID', 'survey_response');
+		$this->dropColumn('survey_response', 'survey_ID');
+		
+		$this->dropForeignKey('survey_response_ibfk_2', 'survey_response');
+		$this->dropIndex('survey_question_ID', 'survey_response');
+		$this->dropColumn('survey_response', 'survey_question_ID');
+	}
+
+	public function down()
+	{
+		$this->addColumn('survey_response', 'survey_id', 'int(11) not null');
+		$this->addForeignKey('survey_response_ibfk_1', 'survey_response', 'survey_id', 'survey', 'id','CASCADE','CASCADE');
+		$this->createIndex('survey_id', 'survey_response', 'survey_ID');
+		
+		$this->addColumn('survey_response', 'survey_question_id', 'int(11) not null');
+		$this->addForeignKey('survey_response_ibfk_2', 'survey_response', 'survey_question_id', 'survey_question', 'id','CASCADE','CASCADE');
+		$this->createIndex('survey_question_id', 'survey_response', 'survey_question_ID');
+		
+	}
+		
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
