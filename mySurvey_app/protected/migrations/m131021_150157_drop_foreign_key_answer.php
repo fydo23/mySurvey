c74071fb500a873084f0706f355a91bcde841204
fwolf <fwolf@bu.edu>
2013-10-21 21:25:55 -0400

    fixed migration down to mirror the up command.

--- mySurvey_app/protected/migrations/m131021_150157_drop_foreign_key_answer.php
+++ mySurvey_app/protected/migrations/m131021_150157_drop_foreign_key_answer.php
@@ -12,8 +12,8 @@ class m131021_150157_drop_foreign_key_answer extends CDbMigration
 	public function down()
 	{
 		$this->addColumn('survey_answer', 'survey_id', 'int(11) not null');
-		$this->addForeignKey('survey_answer_ibfk_1', 'survey_answer', 'survey_id', 'survey', 'id','CASCADE','CASCADE');
 		$this->createIndex('survey_id', 'survey_answer', 'survey_ID');
+		$this->addForeignKey('survey_answer_ibfk_1', 'survey_answer', 'survey_id', 'survey', 'id','CASCADE','CASCADE');
 
 	}
 
