508f9b827b5e03542568102d3ce95ae481cf3d7c
dualv <dualv@bu.edu>
2013-10-28 09:07:07 -0400

    Revert "update view on dropdownlist change"
    This reverts commit 94d155332f37a2bf63e27df2d90e28fae5194ee3.

--- mySurvey_app/themes/mySurvey_001/views/question/_form.php
+++ mySurvey_app/themes/mySurvey_001/views/question/_form.php
@@ -21,21 +21,16 @@
 	
 	<div class="row">
 		<?php echo $form->labelEx($model,'type'); ?>
-		<?php echo $form->dropDownList($model,'type', array(1=>'Short Answer', 2=>'Multiple Choice'),array('ajax'=>array(
-			'type'=>'POST',
-			'url'=>CController::createUrl('/question/update/1'),
-			'data'=>array('type'=>'js:this.value'),
-			'success'=>'function(data){ location.reload();}'
-		)));?>
+		<?php echo $form->dropDownList($model,'type', array(1=>'Short Answer', 2=>'Multiple Choice')); ?>
 		<?php echo $form->error($model,'type'); ?>
 	</div>
-        <?php if($model->type==1){?>
+        
 	<div class="row">
 		<?php echo $form->labelEx($model,'text'); ?>
 		<?php echo $form->textField($model,'text',array('size'=>60,'maxlength'=>1000)); ?>
 		<?php echo $form->error($model,'text'); ?>
 	</div>
-<?php }else if($model->type==2){ echo 'multiple choice';}?>
+
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
