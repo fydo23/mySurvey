1e8f22b60e3cc621cdf4a9dfd238f3fdadd34a86
fwolf <fwolf@bu.edu>
2013-10-29 21:22:43 -0400

    moved css for survey form into main.css

--- mySurvey_app/themes/mySurvey_001/views/survey/update.php
+++ mySurvey_app/themes/mySurvey_001/views/survey/update.php
@@ -4,56 +4,6 @@
 /* @var $questions_dataProvider CActiveDataProvider*/
 
 ?>
-<style>
-    
-    
-    [draggable="true"]{
-        -moz-user-select: none;
-        -khtml-user-select: none;
-        -webkit-user-select: none;
-        user-select: none;
-        /* Required to make elements draggable in old WebKit */
-        -khtml-user-drag: element;
-        -webkit-user-drag: element;
-    }
-    .question_summary{
-        height:10px;
-        width:500px; 
-        border:1px solid #5887A2;
-        background:#A1DCE7;
-        padding: 8px 20px;
-        margin: 5px 0px;
-    }
-    #sortable li{
-        cursor: move;
-    }
-    .question_summary .order_number{
-        float: left;
-    }
-    .question_summary a{
-        float:right;
-        margin-left: 10px;
-    }
-    .dragging{
-        border: 2px dashed;
-        margin:-1px;
-        background:#E4F5F8;
-    }
-    input[name="Survey[title]"]{
-        font-size: 23px;
-        width:530px;
-    }
-    .success input{
-        border-color: green;
-    }
-    .error .errorMessage, .error .arrow-left{
-        margin-top:6px;
-    }
-    .error .errorMessage{
-        padding:7px 15px;
-    }
-</style>
-
 
 <h1>Edit Survey:</h1>
 
