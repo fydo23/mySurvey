82d1e913fdcb7a30de2763e8656a39d51c86a78b
Chao-Te Yang <andrewy@bu.edu>
2013-10-31 13:41:20 -0400

    Modified LoginTest

--- mySurvey_app/protected/tests/functional/LoginTest.php
+++ mySurvey_app/protected/tests/functional/LoginTest.php
@@ -1,73 +1,43 @@
-<?php
-class LoginTest extends PHPUnit_Extensions_Selenium2TestCase {
+<?
+/**
+* 
+*/
+class ClassName extends PHPUnit_Extensions_Selenium2TestCase {
+	
+	private $url = "http://localhost/mySurvey/mySurvey_app";
+	private $email = "aaa@aaa.com";
+	private $password = "12341234";
 	
 	protected function setUp() {
 		// configure selenium server (depends on your machine)
 		$this->setHost ( "localhost" );
 		$this->setBrowser ( "firefox" );
 		$this->setPort ( 4444 );
-		$this->setBrowserUrl ( "http://localhost/mySurvey/mySurvey_app" );
+		$this->setBrowserUrl ( $this->url );
 	}
 	
-	public function testHasLoginForm() {
-		// test button
+	public function testLogin() {
 		$this->url ( "index.php" );
-		$this->assertEquals ( "sign-in", $this->byId ( "sign-in" )->attribute ( "id" ) );
-		$this->assertEquals ( "register-link", $this->byId ( "register-link" )->attribute ( "id" ) );
-		
-		// clicked register button
 		$this->clickOnElement ( "sign-in" );
-		$this->assertEquals ( "", $this->byId ( "LoginForm_email" )->value () );
-		$this->assertEquals ( "", $this->byId ( "LoginForm_password" )->value () );
-		sleep(1);
-		
-		// clicked sign in button
-		$this->clickOnElement ( "register-btn" );
-		$this->assertEquals ( "", $this->byId ( "SurveyCreator_email" )->value () );
-		$this->assertEquals ( "", $this->byId ( "SurveyCreator_password" )->value () );
-		$this->assertEquals ( "", $this->byId ( "SurveyCreator_password_repeat" )->value () );
-		sleep(1);
-	}
-	
-	public function testRegister() {
-		$this->url ( "index.php" );
-		$this->clickOnElement ( "register-link" );
 		
 		// retrieve form information
-		$form = $this->byId ( "register-form" );
+		$form = $this->byId ( "login-form" );
 		$action = $form->attribute ( "action" );
-		$this->assertContains("register", $action);
+		$this->assertContains ( "login", $action );
 		
 		// create a new account and wait for 1 second to load the page
-		$email = "aaa@aaa.com";
-		$this->byId("SurveyCreator_first_name")->value("Test-FirstName");
-		$this->byId("SurveyCreator_last_name")->value("Test-LastName");
-		$this->byId("SurveyCreator_email")->value($email);
-		$this->byId("SurveyCreator_password")->value("12341234");
-		$this->byId("SurveyCreator_password_repeat")->value("12341234");
+		$this->byId ( "LoginForm_email" )->value ( $this->email );
+		$this->byId ( "LoginForm_password" )->value ( $this->password );
+		$form->submit ();
+		sleep ( 1 );
 		
-		try {
-			$form->submit();
-			sleep(1);
-			
-			// after registration, check if the login account is correct
-			// if the user has already been registered, catch the exception
-			$loginAccount = $this->byId("logout")->text();
-			$this->assertContains($email, $loginAccount);
-			
-		} catch (Exception $e) {
-			sleep(1);
-			// check if the error message shows up
-			$this->assertTrue($this->byId("SurveyCreator_email_em_")->displayed());
-		}
-	}
-	
-	public function testLogout(){
-		// test user logout
-	}
-	
-	public function testLogin(){
-		// test user login
+		// after login, check if the login account shows up
+		$loginAccount = $this->byId ( "logout" )->text ();
+		$this->assertContains ( $this->email, $loginAccount );
+		
+		// finally we logout the user
+		$this->byLinkText("Logout")->click();
+		$this->assertTrue($this->byId("login-logout")->displayed());
 	}
 }
 
