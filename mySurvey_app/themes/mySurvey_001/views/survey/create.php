5f2781836a48a113e6e51b7668a334c7c925a83f
Fyodor Wolf <fwolf@bu.edu>
2013-11-01 16:15:35 -0400

    removed un-neccesary view files.

--- mySurvey_app/themes/mySurvey_001/views/survey/create.php
+++ mySurvey_app/themes/mySurvey_001/views/survey/create.php
@@ -10,17 +10,12 @@
 
     <?php $form=$this->beginWidget('CActiveForm', array(
             'id'=>'survey-form',
-            // Please note: When you enable ajax validation, make sure the corresponding
-            // controller action is handling ajax validation correctly.
-            // There is a call to performAjaxValidation() commented in generated controller code.
-            // See class documentation of CActiveForm for details on this.
             'enableAjaxValidation'=>false,
     )); ?>
 
             <p class="note">Fields with <span class="required">*</span> are required.</p>
 
             <?php echo $form->errorSummary($model); ?>
-
 
             <div class="row">
                     <?php echo $form->labelEx($model,'title'); ?>
