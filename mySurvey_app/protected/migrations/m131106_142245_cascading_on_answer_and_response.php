768bf5d9faf059df3ee2e4a1fe4dc7ed0f2a915a
fwolf <fwolf@bu.edu>
2013-11-06 09:34:31 -0500

    added reponse fix to previous migration.

--- mySurvey_app/protected/migrations/m131106_142245_cascading_on_answer_and_response.php
+++ mySurvey_app/protected/migrations/m131106_142245_cascading_on_answer_and_response.php
@@ -1,11 +1,14 @@
 <?php
 
-class m131106_142245_added_cascading_to_survey_answer extends CDbMigration
+class m131106_142245_cascading_on_answer_and_response extends CDbMigration
 {
 	public function up()
 	{  
                 $this->dropForeignKey('survey_answer_ibfk_2', 'survey_answer');
                 $this->addForeignKey('survey_answer_ibfk_2', 'survey_answer', 'survey_question_ID', 'survey_question', 'id', 'CASCADE', 'CASCADE');
+
+                $this->dropForeignKey('survey_response_ibfk_3', 'survey_response');
+                $this->addForeignKey('survey_response_ibfk_3', 'survey_response', 'survey_answer_ID', 'survey_answer', 'id', 'CASCADE', 'CASCADE');
 //                
 
 	}
@@ -14,6 +17,9 @@ class m131106_142245_added_cascading_to_survey_answer extends CDbMigration
 	{
                 $this->dropForeignKey('survey_answer_ibfk_2', 'survey_answer');
                 $this->addForeignKey('survey_answer_ibfk_2', 'survey_answer', 'survey_question_ID', 'survey_question', 'id');
+
+                $this->dropForeignKey('survey_response_ibfk_3', 'survey_response');
+                $this->addForeignKey('survey_response_ibfk_3', 'survey_response', 'survey_answer_ID', 'survey_answer', 'id');
 	}
 
 	/*
