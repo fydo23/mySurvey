1a2810ee47c5930c38da4cbe39f1b636fba914b2
dualv <dualv@bu.edu>
2013-11-22 18:03:59 -0500

    Added contact page support

---
+++ mySurvey_app/themes/mySurvey_001/views/site/pages/contact.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this SiteController */
+/* @var $model ContactForm */
+/* @var $form CActiveForm */
+Yii::app()->controller->layout = 'home'; 
+$this->pageTitle=Yii::app()->name . ' - Contact Us';
+$this->breadcrumbs=array(
+	'Contact',
+);
+?>
+
+<h1>Contact Page</h1>
